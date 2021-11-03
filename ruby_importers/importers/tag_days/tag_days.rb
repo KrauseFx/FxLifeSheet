cf2a42fa16ba89a921fc893c14c7bb66542168b8
Felix Krause <krausefx@gmail.com>
2021-11-03 17:00:49 +0100

    Add date verification for `tag_days`

--- ruby_importers/importers/tag_days/tag_days.rb
+++ ruby_importers/importers/tag_days/tag_days.rb
@@ -22,6 +22,59 @@ module Importers
         all_dates[row[:key]][timestamp_date.to_date] << row
       end
 
+      # Verify we have at least one entry for every date
+      # We still have the assumption, that we track alcoholIntake for every day
+      key_to_use_to_verify = "alcoholIntake"
+      current_date = all_dates[key_to_use_to_verify].keys.min
+      
+      dates_with_missing_data = []
+      dates_with_duplicate_data = []
+      
+      while current_date < all_dates[key_to_use_to_verify].keys.max
+        current_date += 1
+        
+        matched = Array(all_dates[key_to_use_to_verify][current_date])
+        if matched.count == 0
+          dates_with_missing_data << current_date
+        elsif matched.count > 1
+          dates_with_duplicate_data << current_date
+        end
+      end
+
+      # On 2021-11-03 we have a total of 21 dates_with_missing_data.count and 3 dates_with_duplicate_data.count
+      # I verified that those dates were messy around time zones, and did not come from any miscalculated dates
+      # In 2021 there were a total of 5 missed dates, and in 2020 only 3
+      if dates_with_missing_data.count > 21 || dates_with_duplicate_data.count > 3
+        # Missing:
+        # => [#<Date: 2019-05-29 ((2458633j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-07 ((2458642j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-08 ((2458643j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-09 ((2458644j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-10 ((2458645j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-11 ((2458646j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-13 ((2458648j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-06-15 ((2458650j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-07-26 ((2458691j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-08-02 ((2458698j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-08-31 ((2458727j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-10-11 ((2458768j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2019-10-12 ((2458769j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2020-03-14 ((2458923j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2020-07-10 ((2459041j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2020-12-05 ((2459189j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2021-03-06 ((2459280j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2021-06-28 ((2459394j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2021-06-29 ((2459395j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2021-06-30 ((2459396j,0s,0n),+0s,2299161j)>,
+        #   #<Date: 2021-11-03 ((2459522j,0s,0n),+0s,2299161j)>]
+        #
+        # Duplicates:
+        # => [#<Date: 2019-06-16 ((2458651j,0s,0n),+0s,2299161j)>, #<Date: 2020-12-06 ((2459190j,0s,0n),+0s,2299161j)>, #<Date: 2021-06-27 ((2459393j,0s,0n),+0s,2299161j)>]
+        require 'pry'; binding.pry
+        raise "Verify the above"
+      end
+
+      # Now, update all the entries
       all_dates.each do |key, key_values|
         key_values.each do |date, matched_rows|
           matched_rows.each do |row|
