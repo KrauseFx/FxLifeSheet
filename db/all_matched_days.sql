8d58f162f72a66d24fc0f27ed5b20b5eaa6a68cf
Felix Krause <krausefx@gmail.com>
2021-11-08 16:12:59 +0100

    Add all_matched_days.sql

---
+++ db/all_matched_days.sql
@@ -0,0 +1,6 @@
+ SELECT raw_data.matcheddate,
+    count(*) AS count
+   FROM raw_data
+  WHERE raw_data.matcheddate IS NOT NULL
+  GROUP BY raw_data.matcheddate
+  ORDER BY raw_data.matcheddate DESC;
