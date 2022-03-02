2b9d91717617ca2badc2d93fe384b05f21a90109
Felix Krause <krausefx@gmail.com>
2022-03-02 13:46:40 +0100

    Remove old log statements

--- visual_playground/frontend/pie_chart.js
+++ visual_playground/frontend/pie_chart.js
@@ -52,8 +52,7 @@ function renderPie(key) {
         };
 
         Plotly.newPlot(`pieGraphAll`, allData, layout, config).then(function(gd) {
-            Plotly.toImage(gd, { height: 900, width: 1000, format: "svg" }).then(function(base64) {
-                console.log(base64)
+            Plotly.toImage(gd, { height: 600, width: 1000, format: "svg" }).then(function(base64) {
                 document.getElementById('svg-export-1').setAttribute("src", base64);
             })
         });
@@ -132,8 +131,8 @@ function renderPieHistoryChart(yearsData, key, groupByMonth, nodeId) {
             textposition: 'inside',
             text: i,
         })
-    }
 
+    }
     var layout = {
         xaxis: groupByMonth ? {
             title: '',
@@ -164,7 +163,6 @@ function renderPieHistoryChart(yearsData, key, groupByMonth, nodeId) {
     };
     Plotly.newPlot(nodeId, data, layout, config).then(function(gd) {
         Plotly.toImage(gd, { height: 650, width: 1000, format: "svg" }).then(function(base64) {
-            console.log(base64)
             document.getElementById('svg-export-' + nodeId).setAttribute("src", base64);
         })
     });
